78ccb21a888534802a65d1208eb4dd132c3bb393
Philip H <philip.habib6347@gmail.com>
2020-05-26 22:49:25 -0400

    modified api() for prod

--- src/services/Api.js
+++ src/services/Api.js
@@ -2,7 +2,6 @@ import axios from 'axios'
 
 export default () => {
   return axios.create({
-    // baseURL: 'https://api--recipes.herokuapp.com/'
-    baseURL: 'http://localhost:8081/'
+     baseURL: 'https://api--recipes.herokuapp.com/'
   })
 }
